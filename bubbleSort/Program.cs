9896ed9a69f44cddb32c2cd657ff48d2e6f701b3
ndalbah <158243596+ndalbah@users.noreply.github.com>
2025-10-07 17:41:59 -0400

    Upload files

---
+++ bubbleSort/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using bubbleSort;
+
+Console.WriteLine("========================= BUBBLE SORT =========================");
+
+//define and display array
+int[] array = { 8, 2, 4, 7, 1, 3, 9, 6, 5 };
+Console.Write("Array before sort:           ");
+Sort.Display(array);
+Console.WriteLine("");
+
+//sort array ascending (smallest to largest)
+Sort.Bubble_Sort_Ascending(array);
+Console.Write("Array sorted ascending:      ");
+Sort.Display(array);
+Console.WriteLine("");
+
+//reset array and define as array2
+int[] array2 = { 8, 2, 4, 7, 1, 3, 9, 6, 5 };
+
+//sort array descending (largest to smallest)
+Sort.Bubble_Sort_Descending(array2);
+Console.Write("Array sorted descending:     ");
+Sort.Display(array2);
+
+Console.WriteLine("\n===============================================================");
---
+++ bubbleSort/Sort.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace bubbleSort
+{
+	public class Sort
+	{
+        //ascending sort
+        public static void Bubble_Sort_Ascending(int[] array)
+		{
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(array, j);
+                    }
+                }
+            }
+        }
+
+        //descending sort
+        public static void Bubble_Sort_Descending(int[] array)
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] < array[j + 1])
+                    {
+                        Swap(array, j);
+                    }
+                }
+            }
+        }
+
+        //swap
+        private static void Swap(int[] array, int j)
+        {
+            int temp = array[j];
+            array[j] = array[j+1];
+            array[j+1] = temp;
+        }
+
+
+        //display
+        public static void Display(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                if (i != array.Length - 1)
+                {
+                    Console.Write(" , ");
+                }
+            }
+        }
+    }
+}
+
